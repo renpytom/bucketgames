87f2bb5de852acae71e743639a7a997de6a8eb73
Tom Rothamel <tom@rothamel.us>
2025-07-28 00:40:31 -0400

    Change the invocation so command comes after the bucket argument.

--- src/bucketgames/main.py
+++ src/bucketgames/main.py
@@ -29,17 +29,16 @@ from . import upload
 def main():
 
     ap = argparse.ArgumentParser(description="Bucket Games - Self-Host Games on S3-Compatible Storage")
+    ap.add_argument("bucket", help="The path to the bucket to initialize or use.")
+
     sps = ap.add_subparsers(dest="command", required=True)
 
     sp = sps.add_parser("upload", help="Sync local directory to S3-compatible storage")
-    sp.add_argument("bucket", help="The name of the bucket to sync up.")
 
     sp = sps.add_parser("generate", help="Generate website files for a bucket")
-    sp.add_argument("bucket", help="The name of the bucket to generate website files for.")
     sp.add_argument("--serve", action="store_true", help="Serve the generated website files immediately after generation.")
 
     sp = sps.add_parser("serve", help="Run the web server to serve the website files")
-    sp.add_argument("bucket", help="The name of the bucket to server.")
 
     args = ap.parse_args()
 
